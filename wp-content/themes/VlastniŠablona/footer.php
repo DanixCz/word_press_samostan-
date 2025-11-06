306d73aade50af15305b35d1b8b5acfcfe4d9d97
Daniel Zeman <Daniel.Zeman@student.souepl.cz>
2025-11-06 11:28:14 +0100

    3

---
+++ wp-content/themes/VlastniŠablona/footer.php
@@ -0,0 +1,45 @@
+<footer class="footer" >
+	<div class="waves">
+		<div class="wave" id="wave1"></div>
+		<div class="wave" id="wave2"></div>
+		<div class="wave" id="wave3"></div>
+		<div class="wave" id="wave4"></div>
+	</div>
+
+    <div class="wrapper">
+        
+        <div class="wrapper-content">
+            <h2>Zde se nachazí Footer</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                 Ipsa, suscipit. Quis, pariatur? Sed, architecto ullam ex dolorum
+                  odio accusantium rerum recusandae error totam molestias nemo omnis
+                   nostrum deserunt esse autem?</p>
+            <div class="wrapper-content-social">
+                <a href="#">
+
+                </a>
+                <a href="#">
+
+                </a>
+                <a href="#">
+
+                </a>
+                <a href="#">
+
+                </a>
+            </div>    
+        </div>
+        <div class="wrapper-footer">
+            <div class="wrapper-footer-left">
+                <p>© 2024 Všechna práva vyhrazena</p>
+            </div>
+            <div class="wrapper-footer-right">
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+    
+</footer>
