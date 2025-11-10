20927e1243054c3b8f0324d6b1ac8a4b97411a68
Daniel Zeman <Daniel.Zeman@student.souepl.cz>
2025-11-10 13:44:11 +0100

    6

--- wp-content/themes/VlastniŠablona/header.php
+++ wp-content/themes/VlastniŠablona/header.php
@@ -1,9 +1,9 @@
 <header>
     <div class="wrapper">
-<!--        <div class="wrapper-logo">
+        <div class="wrapper-logo">
             <?php /* Use theme directory URL so image works regardless of site root */ ?>
-            <img src="<?php echo esc_url( get_template_directory_uri() . '/img/Blue.png' ); ?>" alt="<?php bloginfo( 'name' ); ?>">
-        </div>-->
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/img/Blue.svg' ); ?>" alt="<?php bloginfo( 'name' ); ?>">
+        </div>
         <div class="wrapper-hamburger">
             <span></span>
             <span></span>
